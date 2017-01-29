8179dd6d552cfba43abb24d1cfbc9a9840a7dbe9
unknown <>
2017-01-29 19:31:14 +0900

    クラスの作成1提出用

--- CampChallenge_java/005.オブジェクト指向１/obj3.java
+++ CampChallenge_java/005.オブジェクト指向１/obj3.java
@@ -10,5 +10,21 @@ package objkadai;
  * @author PCUser
  */
 public class obj3 {
+    //パブリックな変数
+    public String hensu1 = "";
+    public int hensu2 = 0;
     
+    //変数に情報を入れるメソッド
+  public String meth1(){
+		hensu1 = "テスト";
+		return hensu1;
+                            }
+  public int meth2(){
+		hensu2 = 1;
+		return hensu2;
 }
+  public void pri(){
+ System.out.println(hensu1+hensu2);
+}
+    
+                }
