a7d3277b3a475ac9bd68979e13a3e030f4aa1625
KasagiTakuya <3gita46@gmail.com>
2017-01-29 20:03:18 +0900

    Update obj3.java
    クラスの作成提出用

--- CampChallenge_java/005.オブジェクト指向１/obj3.java
+++ CampChallenge_java/005.オブジェクト指向１/obj3.java
@@ -1,30 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package objkadai;
-
-/**
- *
- * @author PCUser
- */
-public class obj3 {
+public class obj3{
     //パブリックな変数
-    public String hensu1 = "";
-    public int hensu2 = 0;
+    public static String hensu1 = "";
+    public static int hensu2 = 0;
     
     //変数に情報を入れるメソッド
-  public String meth1(){
-		hensu1 = "テスト";
-		return hensu1;
+  public  void meth1(String b){
+		hensu1 = b;
+		
                             }
-  public int meth2(){
-		hensu2 = 1;
-		return hensu2;
+  public  void meth2(int a){
+		hensu2 = a;
+		
 }
-  public void pri(){
+  public  void pri(){
  System.out.println(hensu1+hensu2);
-}
+  }
     
                 }
