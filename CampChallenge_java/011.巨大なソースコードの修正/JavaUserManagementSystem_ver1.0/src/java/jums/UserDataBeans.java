4464bd3388a452625932a9b68c6e8192c86e9852
unknown <>
2017-02-13 13:11:00 +0900

    ソース修正第1弾 3.javabeans作成

---
+++ CampChallenge_java/011.巨大なソースコードの修正/JavaUserManagementSystem_ver1.0/src/java/jums/UserDataBeans.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jums;
+
+/**
+ *
+ * @author PCUser
+ */
+public class UserDataBeans {
+    
+           private String name;
+           private String year;
+           private String month;
+           private String day;
+                   /*int? */
+           private String type;
+           private String tell;
+           private String comment;
+           
+           public String getName(){
+           return name;
+           }
+           public void setName(String name){
+           this.name = name;
+           }
+           
+           public String getyear(){
+           return year;
+           }
+           public void setyear(String year){
+           this.year = year;
+           }
+           
+           public String getmonth(){
+           return year;
+           }
+           public void setmonth(String month){
+           this.month = month;
+           }
+           
+           public String getday(){
+           return year;
+           }
+           public void setday(String day){
+           this.year = day;
+           }
+           
+           public String gettype(){
+           return year;
+           }
+           public void settype(String type){
+           this.type = type;
+           }
+           
+           public String gettell(){
+           return tell;
+           }
+           public void settell(String tell){
+           this.tell = tell;
+           }
+           
+           public String getcomment(){
+           return comment;
+           }
+           public void setcomment(String comment){
+           this.comment = comment;
+           }
+    
+}
