e473a31e0dddc70590e0fb2c4d800aa71fb83dab
unknown <>
2017-02-20 13:00:14 +0900

    ソース修正第1弾 2/20 メモ帳に説明があります

---
+++ CampChallenge_java/011.巨大なソースコードの修正/JavaUserManagementSystem_ver1.1/src/java/jums/UserDataBeans.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jums;
+
+
+
+/**
+ *
+ * @author PCUser
+ */
+public class UserDataBeans{
+    
+    
+           private String name;
+           private String year;
+           private String month;
+           private String day;
+                   /*int? */
+           private String type;
+           private String tell;
+           private String comment;
+           
+        public UserDataBeans(){
+           this.name = "";
+           this.year = "";
+           this.month = "";
+           this.day = "";
+           this.type = "";
+           this.tell = "";
+           this.comment = "";
+                             }
+
+           
+           public String getName(){
+           return name;
+           }
+           public void setName(String name){
+           this.name = name;
+           }
+           
+           public String getyear(){
+           return year;
+           }
+           public void setyear(String year){
+           this.year = year;
+           }
+           
+           public String getmonth(){
+           return month;
+           }
+           public void setmonth(String month){
+           this.month = month;
+           }
+           
+           public String getday(){
+           return day;
+           }
+           public void setday(String day){
+           this.day = day;
+           }
+           
+           public String gettype(){
+           return type;
+           }
+           public void settype(String type){
+           this.type = type;
+           }
+           
+           public String gettell(){
+           return tell;
+           }
+           public void settell(String tell){
+           this.tell = tell;
+           }
+           
+           public String getcomment(){
+           return comment;
+           }
+           public void setcomment(String comment){
+           this.comment = comment;
+           }
+    
+}
